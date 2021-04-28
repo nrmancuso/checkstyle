0a2358422edfeae6cdcc837e2e0d52bb744de826
Nick Mancuso <nmancus1@dtcc.edu>
2021-04-28 10:56:38 -0400

    f

--- runner.sh
+++ runner.sh
@@ -5,5 +5,7 @@ do
     OUTPUT=$(.ci/no-exception-test.sh openjdk14-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh '/.*.java' | sed 's|.*/||' | cut -f 1 -d '.' | uniq)
     echo "$OUTPUT"
     echo -e "<module name=\"BeforeExecutionExclusionFileFilter\">\n  <property name=\"fileNamePattern\" value=\"/$OUTPUT\.java\$\"/>\n</module>" >> config/openjdk14-excluded.files
-    rm -rf .ci-temp/contribution
+    #rm -rf .ci-temp/contribution
+    git add .
+    git commit -m "add filter"
 done
