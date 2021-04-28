43d0f4a6af9a00e5e004112badc7dcd99715a01d
Nick Mancuso <nmancus1@dtcc.edu>
2021-04-28 11:26:58 -0400

    f

--- runner.sh
+++ runner.sh
@@ -2,7 +2,7 @@
 OUTPUT="test"
 while  [ -n "$OUTPUT" ]
 do
-    OUTPUT=$(.ci/no-exception-test.sh openjdk14-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh '([A-z0-9-_+]+\/)*([A-z0-9]+\.(java))' | sed 's|.*/||' | cut -f 1 -d '.' | uniq)
+    OUTPUT=$(.ci/no-exception-test.sh openjdk14-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh "([A-z0-9-_+]+\/)*([A-z0-9]+\.(java))" | sed 's|.*/||' | cut -f 1 -d '.' | uniq)
     echo "$OUTPUT"
     echo -e "<module name=\"BeforeExecutionExclusionFileFilter\">\n  <property name=\"fileNamePattern\" value=\"[\\/]$OUTPUT\.java\$\"/>\n</module>" >> config/openjdk14-excluded.files
     rm -rf .ci-temp/contribution
