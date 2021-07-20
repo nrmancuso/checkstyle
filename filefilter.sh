51eeff23e55e6afaed296528f6f392bb86541d26
Nick Mancuso <nmancus1@dtcc.edu>
2021-07-20 13:31:18 -0400

    temp

--- filefilter.sh
+++ filefilter.sh
@@ -2,7 +2,7 @@
 OUTPUT="test"
 while  [ -n "$OUTPUT" ]
 do
-    OUTPUT=$(.ci/no-exception-test.sh openjdk14-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh '/.*.java' | sed 's./.[\\\\/].g' | uniq)
+    OUTPUT=$(.ci/no-exception-test.sh openjdk14-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh '/.*.java' | sed 's./.[\\\\\\/].g' | uniq)
     echo "$OUTPUT"
     echo -e "<module name=\"BeforeExecutionExclusionFileFilter\">\n  <property name=\"fileNamePattern\" value=\"/$OUTPUT\$\"/>\n</module>" >> .ci/openjdk14-excluded.files
     rm -rf .ci-temp/contribution
