17a564897de88e1f330304040325bbe98ed8f9eb
Nick Mancuso <nmancus1@dtcc.edu>
2021-07-20 13:01:55 -0400

    temp

--- filefilter.sh
+++ filefilter.sh
@@ -2,9 +2,9 @@
 OUTPUT="test"
 while  [ -n "$OUTPUT" ]
 do
-    OUTPUT=$(.ci/no-exception-test.sh openjdk14-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh '/.*.java' | sed 's|.*/||' | cut -f 1 -d '.' | uniq)
+    OUTPUT=$(.ci/no-exception-test.sh openjdk14-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh '/.*.java' | sed 's./.[\\\\/].g' | uniq)
     echo "$OUTPUT"
-    echo -e "<module name=\"BeforeExecutionExclusionFileFilter\">\n  <property name=\"fileNamePattern\" value=\"/$OUTPUT\.java\$\"/>\n</module>" >> .ci/openjdk14-excluded.files
+    echo -e "<module name=\"BeforeExecutionExclusionFileFilter\">\n  <property name=\"fileNamePattern\" value=\"/$OUTPUT\$\"/>\n</module>" >> .ci/openjdk14-excluded.files
     rm -rf .ci-temp/contribution
     git add .ci/openjdk14-excluded.files
     git commit -m "added another file filter"
